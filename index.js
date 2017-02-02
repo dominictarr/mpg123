e977b2c07cf7f1aa4cb0a561c24cec3c7c4fab74
Bryce <Pecacheu@gmail.com>
2017-02-02 04:52:28 -0500

    Select Output Device
    Added output device selection, as well as some other cool features!

--- index.js
+++ index.js
@@ -5,74 +5,114 @@ var es           = require('event-stream');
 var through      = require('through');
 
 inherits(MpgPlayer, EventEmitter);
+exports.MpgPlayer = MpgPlayer;
 
-module.exports = MpgPlayer;
-function MpgPlayer() {
-    var self = this;
-    this.child = spawn('mpg123', ['-R']);
-    this.stream = this.child.stdin,
-    this.child.stdout
-        .pipe(es.split())
-        .pipe(through(function (data) {
-            var line = data.split(' ');
-            var type = line.shift();
+exports.getDevices = function(callback) {
+	execCmd('aplay -L', function(raw) {
+		var lines = raw.split('\n'), devices = [];
+		for(var i=0,l=lines.length; i<l; i++) {
+			var line = lines[i]; if(line[0] == 'h' && line[1] == 'w' && line[2] == ':') {
+				var name = lines[i+1]; if(name) {
+					name = name.substring(0,name.indexOf(',')).trim();
+					if(debug) console.log("DEV: '"+line+"' is '"+name+"'");
+					devices.push({name:name,address:line});
+				}
+			}
+		}
+		devices.get = function(n) {
+			for(var i=0,l=this.length; i<l; i++) if(this[i].name == n) return this[i];
+			return null;
+		};
+		callback(devices);
+	});
+}
 
-            switch(type){
-                case '@P':
-                    var event = ['end', 'pause', 'resume'][+line.shift()];
-                    self.emit(event);
-                    break;
-                case '@E':
-                    var err = new Error(line.join(' '));
-                    err.type = 'mpg-player';
-                    self.emit('error', err);
-                    break;
-                case '@F':
-                    self.emit('frame', line);
-                    break;
-                case '@J':
-                    self.emit('jump');
-                    break;
-                case '@V':
-                    var percent = line[0].substring(0, line[0].length-1);
-                    self.emit('volume', percent);
-                    break;
-            }
-        }));
+function MpgPlayer(device, noFrames) {
+	var self = this, args = ['-R'];
+	if(typeof device == 'object') args.push('-a'+device.address);
+	
+	this.child = spawn('mpg123', args); this.stream = this.child.stdin;
+	if(noFrames) this._cmd('SILENCE');
+	
+	this.child.stdout.pipe(es.split()).pipe(through(function(data) {
+		var line = data.split(' '), type = line.shift();
+		switch(type) {
+		case '@P':
+			var event = ['end', 'pause', 'resume'][+line.shift()];
+			self.emit(event);
+		break; case '@E':
+			var msg = line.join(' '), err = new Error(msg); err.type = 'mpg-player';
+			if(msg.indexOf("No stream opened") != -1) {
+				for(var i=0,l=self._gpcb.length; i<l; i++) self._gpcb[i](0,0,0);
+				self._gpcb = [];
+			}
+			self.emit('error', err);
+		break; case '@F':
+			self.emit('frame', line);
+		break; case '@J':
+			self.emit('jump');
+		break; case '@V':
+			var per = line[0]; per = per.substring(0, per.length-1);
+			self.emit('volume', per);
+		break; case '@S':
+			if(self._s == 1) {
+				self.mpeg = Number(line[0]);
+				self.sampleRate = line[2];
+				self.channels = Number(line[4]);
+				self.bitrate = Number(line[10]);
+				self._s = 2; this._cmd('SAMPLE');
+			}
+		break; case '@SAMPLE':
+			if(self._s == 2) {
+				self.samples = line[1];
+				self.length = Math.round((self.samples/self.sampleRate)*10)/10;
+				self._s = 0; self.emit('format');
+			}
+			var s = line[0], l = line[1], p = (s/l);
+			for(var i=0,l=self._gpcb.length; i<l; i++) self._gpcb[i](p,s,l);
+			self._gpcb = [];
+		break;
+		}
+	}));
 }
 
 var p = MpgPlayer.prototype;
 
-p._cmd = function () {
-    var args = [].slice.call(arguments);
-    this.stream.write(args.join(' ') + '\n');
-    return this;
-};
-
-p.play = function (file) {
-    return this._cmd('LOAD', file);
-};
-
-p.pause = function () {
-    return this._cmd('PAUSE');
-};
-
-p.stop = function () {
-    return this._cmd('STOP');
-};
-
-p.volume = function (vol) {
-    vol = Math.min(Math.max(vol, 0), 100);
-    return this._cmd('VOLUME', vol);
-};
-
-
-p.close = function () {
-    this.child.kill();
-};
-
+p._cmd = function() {
+	var args = [].slice.call(arguments);
+	this.stream.write(args.join(' ') + '\n');
+	return this;
+}
+p.play = function(file) {
+	if(file[0] != '/' && file[1] != ':') file = __dirname+'/'+file;
+	this.track = file; return this._cmd('L', file); this._s = 1;
+}
+p.pause = function() {
+	return this._cmd('P');
+}
+p.stop = function() {
+	return this._cmd('S');
+}
+p.pitch = function(pitch) {
+	pitch = Math.min(Math.max(pitch, -0.75), 0.1);
+	return this._cmd('PITCH', pitch);
+}
+p.volume = function(vol) {
+	vol = Math.min(Math.max(vol, 0), 100);
+	return this._cmd('V', vol);
+}
+p.seek = function(pos) {
+	pos = Math.min(Math.max(pos, 0), 1);
+	return this._cmd('K', Math.floor(pos*this.samples));
+}
+p.getProgress = function(callback) {
+	this._gpcb.push(callback);
+	return this._cmd('SAMPLE');
+}
+p.close = function() {
+	this.child.kill();
+}
 
 if(!module.parent) {
-    new MpgPlayer()
-        .play(process.argv[2]);
+	new MpgPlayer().play(process.argv[2]);
 }
